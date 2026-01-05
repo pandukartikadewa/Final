5b0f3eed38503941aeba81b4c069d245ac723335
aplis101 <2406016105@webmail.uad.ac.id>
2026-01-05 21:58:57 +0700

    Checkpoint 3: Admin CRUD + Login Signup + Auth Simulation

--- src/components/admin/DataTable.jsx
+++ src/components/admin/DataTable.jsx
@@ -1,6 +1,19 @@
 import { Link } from "react-router-dom";
+import { deleteEvent } from "../../services/eventService";
 
-export default function DataTable({ events }) {
+export default function DataTable({ events, onDelete }) {
+  const handleDelete = async (id) => {
+    const confirmDelete = confirm("Yakin ingin menghapus event?");
+    if (!confirmDelete) return;
+
+    try {
+      await deleteEvent(id);
+      onDelete(id);
+    } catch (error) {
+      alert("Gagal menghapus event");
+    }
+  };
+
   return (
     <table className="w-full border border-gray-300">
       <thead className="bg-gray-200">
@@ -15,20 +28,27 @@ export default function DataTable({ events }) {
         {events.map((event) => (
           <tr key={event.id}>
             <td className="border p-2 text-center">
-              {event.name}
+              {event.title}
             </td>
 
             <td className="border p-2 text-center">
-              Rp {event.price.toLocaleString()}
+              Rp {Number(event.price).toLocaleString("id-ID")}
             </td>
 
-            <td className="border p-2 text-center">
+            <td className="border p-2 text-center space-x-2">
               <Link
                 to={`/admin/events/edit/${event.id}`}
-                className="inline-block bg-red-600 text-white px-3 py-1 rounded"
+                className="inline-block bg-blue-600 text-white px-3 py-1 rounded"
               >
                 Edit
               </Link>
+
+              <button
+                onClick={() => handleDelete(event.id)}
+                className="inline-block bg-red-600 text-white px-3 py-1 rounded"
+              >
+                Delete
+              </button>
             </td>
           </tr>
         ))}
