c8be85a8d2f509627f99a88d9d39778419c112a8
pandukartikadewa <pandukartikadewa01@gmail.com>
2026-01-13 19:48:16 +0700

    Delete src/pages/ForgotPassword.jsx

--- src/pages/ForgotPassword.jsx
+++
@@ -1,7 +0,0 @@
-export default function ForgotPassword() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <p>Lupa Password (UI saja)</p>
-    </div>
-  );
-}
